44b5ff45690374618729cba507ea1052b065d0b0
Jazzzim <jasim.khan89@gmail.com>
2023-03-30 18:15:40 -0400

    update project files

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -4,54 +4,21 @@
 
 (function()
 {
-    /**
-     *This function loads data Asynchronously from a URL. 
-    * It calls the callback function when the data is loading
-     * @param {string} method
-     * @param {string} url
-     * @param {function} callback
-     */
-    function LoadData(method: string,url: string,callback:Function ): void {
-    {
-        //Step 1 create a new empty XHR object
-            let XHR = new XMLHttpRequest();
-        //Step 2 - Compose a request
-            XHR.open(method, url);
-        // Step 3 - Send the request to the server
-            XHR.send();
-
-        //Step 4 - setup an event listener
-        XHR.addEventListener("readystatechange",function(){
-
-            if((XHR.status == 200) && (XHR.readyState == 4))
-            {
-                callback(XHR.responseText);
-            }
-        });
-    }
+   
 
     //Named function
      function Start()
      {
              console.log("App started!!");
 
-            //  LoadData("GET","./Data/contacts.json",function(XHR){
-            //         console.log(XHR);
-            //  });
+      
 
-             $.getJSON("./Data/contacts.json",function(data){
-                console.log(data);
+             $.getJSON("./Data/contacts.json",function(DataSource){
+                console.log(DataSource);
              });
      }
 
    
     window.addEventListener("load",Start);
 
-};
-
-function callback(responseText: string) {
-    throw new Error("Function not implemented.");
-};
-
-
-})
+})();
